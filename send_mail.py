4139fc9385cbca7de1d466da93700a0206a84dbd
jreznick <jreznick@gmail.com>
2023-06-27 00:21:23 -0400

    Create send_mail.py

---
+++ send_mail.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+from .logging import mylogger
+
+TO_EMAILS = []  # iterate over recipients
+FROM_EMAIL = ''  # there is only one sender address
+
+
+def send_email(mylogger: object, body: str):
+  """
+  :param mylogger: an application logger
+  :param body: your email message body, containing HTML
+  """
+    if body is not None:
+        now = datetime.now()
+        date = now.strftime("%Y-%m-%d")
+        time = now.strftime("%H:%M")
+        subject = f'-{date}-{time}'  # set custom email subject here
+        message = Mail(
+            from_email=FROM_EMAIL,
+            to_emails=TO_EMAILS,
+            subject=subject,
+            html_content=body)
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        try:
+            response = sg.send(message)
+            mylogger.info(f'Email resp: {response.status_code} {date} {time}')
+            success = True
+        except Exception as e:
+            log_line = f'Exception {e}'
+            mylogger.error(log_line)
+            success = False
+
+        return success, response
+    else:
+        mylogger.info(f"There are no new alerts to send.")
+
+        return False, None
