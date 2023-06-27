a5110f636afd5af394afcc0fa4c003e6d7649a17
jreznick <jreznick@gmail.com>
2023-06-27 01:30:13 -0400

    Create __env__.py

---
+++ __env__.py
@@ -0,0 +1,9 @@
+#!/Anaconda3/python
+from pathlib import Path
+
+repo_root = ''
+package_root = ''
+data_root = ''
+PATH_HOME = Path(repo_root)
+PATH_PACKAGE = Path(package_root)
+PATH_DATA = Path(data_root)
